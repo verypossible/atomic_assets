1a7a6626b6c36325bc62c2f8c105374d179efb69
Don Humphreys <dhumphreys88@gmail.com>
2015-06-20 15:31:54 -0400

    Removed unneeded deserialization methods

--- lib/atomic_assets/deserialize.rb
+++ lib/atomic_assets/deserialize.rb
@@ -7,32 +7,12 @@ module AtomicAssets
     end
 
     module ClassMethods
-      def from_hash(params)
-        obj = params.delete(:template_name).constantize.new
-        params.each { |key, val| obj.send("#{key}=", from_value(key, val)) }
-        obj
-      end
-
       def from_yaml(yaml_string)
         preload_components(yaml_string)
         YAML.load(yaml_string)
       end
 
       private
-
-      def from_array(hash)
-        array = []
-        hash.each do |key,element|
-          array << from_hash(element)
-        end
-        array
-      end
-
-      def from_value(key, value)
-        return from_array(value) if key === 'children'
-        return from_hash(value) if Hash === value
-        value
-      end
 
       def preload_components(yaml_string)
         yaml_string.scan(/(?<=\!ruby\/object:)\w+/).uniq.map(&:constantize)
