62b8587b329c4c53a2aa3ca9b834805918e36179
Bill Brock <william.e.brock@gmail.com>
2015-10-07 11:30:18 -0400

    Added file generators for creating a new component
    Why?
    We need a quick way to frame out all the files needed for adding atomic
    assets
    
    How?
    Added a rails specific generator for inserting the required files into
    a rails project. AtomicAssets::Generators::ComponentGenerator. It adds
    the following files to a project:
    * component.rb
    * component.html.slim
    * component.scss
    
    It can be called by the folling commmand:
    rails generate atomic_assets:componenet COMPONENT_NAME
    
    Side Effects?
    Files are generated with sane defaults

---
+++ lib/rails/generators/atomic_assets/component/component_generator.rb
@@ -0,0 +1,38 @@
+module AtomicAssets
+  module Generators
+    class ComponentGenerator < Rails::Generators::Base
+      argument :component_name, required: true
+
+      desc <<-DESC.strip_heredoc
+        Creates all files requred when generating a new atomic asset component
+      DESC
+
+      source_root File.expand_path("../../templates", __FILE__)
+
+      def create_component_file
+        @component_name = class_prefix
+        template "component.rb", "app/components/#{file_prefix}_component.rb"
+      end
+
+      def create_scss_file
+        fullpath = "app/assets/stylesheets/components/_#{file_prefix}.scss"
+        template "component.scss", fullpath
+      end
+
+      def create_slim_file
+        fullpath = "app/views/components/#{file_prefix}.html.slim"
+        template "component.slim", fullpath
+      end
+
+      private
+
+      def file_prefix
+        component_name.underscore
+      end
+
+      def class_prefix
+        component_name.camelize
+      end
+    end
+  end
+end
